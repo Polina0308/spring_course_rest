7c50a6823f53bbc7546fe3be75e586d1191a47cb
polinadavidenko <davidenkopk@gmail.com>
2023-08-29 15:18:43 +0300

    REST API Project

---
+++ src/main/java/com/polina/spring/rest/exception_handling/EmployeeIncorrectData.java
@@ -0,0 +1,13 @@
+package com.polina.spring.rest.exception_handling;
+
+public class EmployeeIncorrectData {
+    private  String info;
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
